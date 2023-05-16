47bae9440d9206462583f1e5fae2c7f2d8a42b5b
madmaxi <madmaxi889@gmail.com>
2023-05-16 19:47:26 +0200

    003 | add ThrowCommand

--- bin/cli.php
+++ bin/cli.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 
 use Madmaxi\Farkle\command\GamesCommand;
 use Madmaxi\Farkle\command\RoundsCommand;
+use Madmaxi\Farkle\command\ThrowCommand;
 use Madmaxi\Farkle\PointsService;
 use Madmaxi\Farkle\RoundService;
 use Madmaxi\Farkle\Service\ThrowService;
@@ -17,10 +18,12 @@ $app = new Application('Farkle');
 $pointsService = new PointsService();
 $throwService = new ThrowService();
 $roundService = new RoundService($pointsService, $throwService);
+
 $gamesCommand = new GamesCommand($roundService);
 $roundsCommand = new RoundsCommand($roundService);
-
+$throwCommand = new ThrowCommand($throwService);
 
 $app->add($gamesCommand);
 $app->add($roundsCommand);
+$app->add($throwCommand);
 $app->run();
